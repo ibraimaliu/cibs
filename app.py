9294dc549361308abc2db712231f058af84a9fb5
ibraimaliu <90778141+ibraimaliu@users.noreply.github.com>
2024-06-20 20:38:43 +0200

    Update app.py rollback 2.1

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 def configure_locale():
     try:
-        # Définir la locale à partir de la variable d'environnement LC_TIME, avec une valeur par défaut 'fr_FR.utf8'
-        locale.setlocale(locale.LC_TIME, os.getenv('LC_TIME', 'fr_FR.utf8'))
+        # Définir la locale à partir de la variable d'environnement LC_TIME, avec une valeur par défaut ''
+        locale.setlocale(locale.LC_TIME, os.getenv('LC_TIME', ''))
     except locale.Error as e:
         print(f"Failed to set locale: {e}")
         # En cas d'échec, utiliser une gestion de fallback ou afficher un message d'erreur
@@ -18,33 +18,15 @@ def configure_locale():
 configure_locale()
 
 def get_prayer_times():
-    # Lire le fichier Excel
-    df = pd.read_excel('data/prayer_times.xlsx')
-
-    # Filtrer les horaires pour la date actuelle
-    today = datetime.date.today()
-    todays_times = df[df['Date'] == pd.to_datetime(today)]
-
-    if not todays_times.empty:
-        prayer_times = todays_times.iloc[0].to_dict()
-        # Formater les horaires sans les secondes
-        for prayer in ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Levé du soleil']:
-            if prayer in prayer_times:
-                # Convertir en chaîne de caractères et enlever les secondes
-                prayer_times[prayer] = prayer_times[prayer].strftime('%H:%M')
-        return prayer_times
-    else:
-        return {}
+    # Votre logique pour récupérer les horaires de prière reste inchangée
+    pass
 
 @app.route('/')
 def index():
     prayer_times = get_prayer_times()
-
-    # Obtenir la date actuelle
+    
     today = datetime.date.today()
-    # Formater la date en français
     formatted_today = today.strftime('%A, %d %B %Y')
-    # Convertir en date hégirienne
     hijri_date = convert.Gregorian.today().to_hijri()
 
     return render_template('index.html', prayer_times=prayer_times, today=formatted_today, hijri_date=hijri_date)
