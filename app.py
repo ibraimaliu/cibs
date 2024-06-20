460015379fd6d8cee7f60d4daacd65f45e454fe1
ibraimaliu <90778141+ibraimaliu@users.noreply.github.com>
2024-06-20 20:52:44 +0200

    Update app.py 2.5 db excel

--- app.py
+++ app.py
@@ -18,36 +18,45 @@ def configure_locale():
 configure_locale()
 
 def get_prayer_times():
-    # Lire le fichier Excel
-    df = pd.read_excel('data/prayer_times.xlsx')
+    try:
+        # Lire le fichier Excel
+        df = pd.read_excel('data/prayer_times.xlsx')
 
-    # Filtrer les horaires pour la date actuelle
-    today = datetime.date.today()
-    todays_times = df[df['Date'] == pd.to_datetime(today)]
+        # Filtrer les horaires pour la date actuelle
+        today = datetime.date.today()
+        todays_times = df[df['Date'] == pd.to_datetime(today)]
 
-    if not todays_times.empty:
-        prayer_times = todays_times.iloc[0].to_dict()
-        # Formater les horaires sans les secondes
-        for prayer in ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Levé du soleil']:
-            if prayer in prayer_times:
-                # Convertir en chaîne de caractères et enlever les secondes
-                prayer_times[prayer] = prayer_times[prayer].strftime('%H:%M')
-        return prayer_times
-    else:
+        if not todays_times.empty:
+            prayer_times = todays_times.iloc[0].to_dict()
+            # Formater les horaires sans les secondes
+            for prayer in ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Levé du soleil']:
+                if prayer in prayer_times:
+                    # Convertir en chaîne de caractères et enlever les secondes
+                    prayer_times[prayer] = prayer_times[prayer].strftime('%H:%M')
+            return prayer_times
+        else:
+            print("No prayer times found for today.")
+            return {}
+    except Exception as e:
+        print(f"Error in get_prayer_times: {e}")
         return {}
 
 @app.route('/')
 def index():
-    prayer_times = get_prayer_times()
+    try:
+        prayer_times = get_prayer_times()
 
-    # Obtenir la date actuelle
-    today = datetime.date.today()
-    # Formater la date en français
-    formatted_today = today.strftime('%A, %d %B %Y')
-    # Convertir en date hégirienne
-    hijri_date = convert.Gregorian.today().to_hijri()
+        # Obtenir la date actuelle
+        today = datetime.date.today()
+        # Formater la date en français
+        formatted_today = today.strftime('%A, %d %B %Y')
+        # Convertir en date hégirienne
+        hijri_date = convert.Gregorian.today().to_hijri()
 
-    return render_template('index.html', prayer_times=prayer_times, today=formatted_today, hijri_date=hijri_date)
+        return render_template('index.html', prayer_times=prayer_times, today=formatted_today, hijri_date=hijri_date)
+    except Exception as e:
+        print(f"Error in index route: {e}")
+        return "An error occurred", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
